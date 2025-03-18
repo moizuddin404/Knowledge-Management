7d8f51b1049a6ced458278aa558141d2ae015bcb
moizuddin404 <shaikhmoizuddin786@gmail.com>
2025-03-18 14:42:51 +0530

    created jwt handler utility funtions and commented code

--- brieffy-backend/app/services/auth_service.py
+++ brieffy-backend/app/services/auth_service.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from database import db_instance
 from models import User
 from config import Config
-import jwt
+from utils import create_access_token, decode_access_token
 
 
 class AuthService:
@@ -15,17 +15,21 @@ class AuthService:
         if existing_user:
             self.users_collection.update_one({"email": user_data.email}, {"$set": {"last_login": datetime.utcnow().isoformat()}}
                                              )
+            user_id = str(existing_user["_id"])
+           
             status = "existing"
         
         else:
             user_data.created_at = datetime.utcnow().isoformat()
             user_data.last_login = datetime.utcnow().isoformat()
-            self.users_collection.insert_one(user_data.dict())
+            result = self.users_collection.insert_one(user_data.dict())
+
+            user_id = str(result.inserted_id)
 
             status = "new"
         
-        token_payload = {"email": user_data.email}
-        token = jwt.encode(token_payload, Config.SECRET_KEY, algorithm="HS256")
+        token_payload = {"userId": user_id,"email": user_data.email}
+        token = create_access_token(token_payload)
 
         return{"message": f"User {status}", "token": token}
         
